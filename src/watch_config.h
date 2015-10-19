8f06477bedd32d0e8bd7e8b486aeb345a57ce885
clach04 <Chris.Clark@actian.com>
2015-10-19 09:43:05 -0400

    Start of Chalk/Round support

--- src/watch_config.h
+++ src/watch_config.h
@@ -6,11 +6,22 @@
 #undef FONT_SYSTEM_NAME  /* the default font system will be used */
 #undef DEBUG_TIME
 
+#ifdef PBL_ROUND /* 180x180 */
+/*TODO center/move right*/
+    #define CLOCK_POS GRect(0, 52, 180, 180) /* probably taller than really needed */
+    #define BT_POS GRect(0, 100, 180, 180) /* probably taller than really needed */
 
-#define CLOCK_POS GRect(0, 52, 144, 168) /* probably taller than really needed */
-#define BT_POS GRect(0, 120, 144, 168) /* probably taller than really needed */
-#define DATE_POS GRect(0, 140, 144, 168) /* probably taller than really needed */
-#define BAT_POS GRect(0, 140, 144, 168) /* probably taller than really needed */
+    #define DATE_ALIGN GTextAlignmentCenter
+    #define DATE_POS GRect(0, 140, 180, 180) /* probably taller than really needed */
+
+    #define BAT_ALIGN GTextAlignmentCenter
+    #define BAT_POS GRect(0, 120, 180, 180) /* probably taller than really needed */
+#else /* PBL_RECT 144x168*/
+    #define CLOCK_POS GRect(0, 52, 144, 168) /* probably taller than really needed */
+    #define BT_POS GRect(0, 120, 144, 168) /* probably taller than really needed */
+    #define DATE_POS GRect(0, 140, 144, 168) /* probably taller than really needed */
+    #define BAT_POS GRect(0, 140, 144, 168) /* probably taller than really needed */
+#endif /* end of Round or rectangle */
 
 /* for screen shots and font testing
 #define DEBUG_TIME
