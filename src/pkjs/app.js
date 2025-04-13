3504c448a50b18f99f4e7131d5cce8f20bbc3d40
clach04 <clach04@gmail.com>
2025-04-13 14:39:17 -0700

    Add start of phone debug for config

--- src/pkjs/app.js
+++ src/pkjs/app.js
@@ -1,3 +1,22 @@
 var Clay = require('pebble-clay');
 var clayConfig = require('./config');
 var clay = new Clay(clayConfig);
+
+// override send config, in case want to modify (change types) values or debug
+Pebble.addEventListener('webviewclosed', function(e) {
+    if (e && !e.response) {
+        return;
+    }
+
+    var dict = clay.getSettings(e.response);
+    //console.log(dict.VIBRATE_ON_DISCONNECT);
+
+    console.log('string config data length=' + JSON.stringify(e).length);
+    // Send settings values to watch side
+    Pebble.sendAppMessage(dict, function(e) {
+        console.log('Sent config data to Pebble');
+    }, function(e) {
+        console.log('Failed to send config data!');
+        console.log(JSON.stringify(e));  // NOTE possibly secret leakage if adding secrets to config
+    });
+});
