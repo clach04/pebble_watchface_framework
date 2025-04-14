eed74388b6f4834452426222ed15f82a89c5146e
clach04 <clach04@gmail.com>
2025-04-13 18:12:31 -0700

    More phone config debug logging

--- src/pkjs/app.js
+++ src/pkjs/app.js
@@ -4,6 +4,7 @@ var clay = new Clay(clayConfig);
 
 // override send config, in case want to modify (change types) values or debug
 Pebble.addEventListener('webviewclosed', function(e) {
+    console.log('webviewclosed entry');
     if (e && !e.response) {
         return;
     }
@@ -11,12 +12,19 @@ Pebble.addEventListener('webviewclosed', function(e) {
     var dict = clay.getSettings(e.response);
     //console.log(dict.VIBRATE_ON_DISCONNECT);
 
-    console.log('string config data length=' + JSON.stringify(e).length);
+    console.log('e string config data length=' + JSON.stringify(e).length);
+    console.log('dict config data length=' + JSON.stringify(dict).length);
+    console.log(JSON.stringify(e));  // NOTE possibly secret leakage if adding secrets to config
+    console.log(JSON.stringify(dict));  // NOTE possibly secret leakage if adding secrets to config
+
     // Send settings values to watch side
+    console.log('DEBUG pre-send');
     Pebble.sendAppMessage(dict, function(e) {
+        console.log('DEBUG success send');
         console.log('Sent config data to Pebble');
     }, function(e) {
-        console.log('Failed to send config data!');
-        console.log(JSON.stringify(e));  // NOTE possibly secret leakage if adding secrets to config
+        console.log('DEBUG FAIL send');
+        console.log('Failed to send data options to Pebble. Error: ' + JSON.stringify(e));  // NOTE possibly secret leakage if adding secrets to config
     });
+    console.log('DEBUG post-send');
 });
